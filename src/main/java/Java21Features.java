b7caa28d80e26c291afac077359c423b08c9a3fc
Kuerm <kuerm@gmx.net>
2024-05-23 16:58:50 +0200

    Add example

--- src/main/java/Java21Features.java
+++ src/main/java/Java21Features.java
@@ -1,15 +1,82 @@
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 public class Java21Features {
 
-    public Set<String> sequencedCollection() {
-        Set<String> strings = new LinkedHashSet<>();
+    public String plainSwitch(Animal animal) {
+        return switch (animal) {
+            case Dog dog -> "dog %s".formatted(dog.name());
+            case Cat cat -> "cat %s".formatted(cat.name());
+            case Mouse __ -> "mouse"; // __ = poor man's unnamed pattern variable
+        };
+    }
 
-        strings.add("first");
+    public String plainSwitchWithDefault(Object object) {
+        return switch (object) {
+            case Dog dog -> "dog %s".formatted(dog.name());
+            case Cat cat -> "cat %s".formatted(cat.name());
+            case Integer i -> "int %d".formatted(i);
+            default -> "unknown";
+        };
+    }
 
-        strings.add("second");
+    public String plainSwitchWithNull(Object object) {
+        return switch (object) {
+            case Dog dog -> "dog %s".formatted(dog.name());
+            case Cat cat -> "cat %s".formatted(cat.name());
+            case null -> "null";
+            default -> "unknown";
+        };
+    }
 
-        return strings;
+    public String plainSwitchWithCondition(Animal animal) {
+        return switch (animal) {
+            case Dog dog when dog.name().equals("Rex") -> "Kommissar Rex";
+            // try to remove next line and see what happens
+            case Dog dog -> "dog %s".formatted(dog.name());
+            case Cat cat -> "cat %s".formatted(cat.name());
+            case Mouse __ -> "mouse";
+        };
+    }
+
+    static final class Cat extends Animal {
+        private Cat(String name) {
+            super(name);
+        }
+
+        public static Cat of(String name) {
+            return new Cat(name);
+        }
+    }
+
+    static final class Mouse extends Animal {
+        private Mouse(String name) {
+            super(name);
+        }
+
+        public static Mouse of(String name) {
+            return new Mouse(name);
+        }
+
+    }
+
+    static final class Dog extends Animal {
+        private Dog(String name) {
+            super(name);
+        }
+
+        public static Dog of(String name) {
+            return new Dog(name);
+        }
+    }
+
+    // try to remove the permits clause and see what happens
+    sealed abstract static class Animal permits Cat, Dog, Mouse {
+        private String name;
+
+        protected Animal(String name) {
+            this.name = name;
+        }
+
+        public String name() {
+            return name;
+        }
     }
 }
--- src/test/java/Java21FeaturesTest.java
+++ src/test/java/Java21FeaturesTest.java
@@ -1,18 +1,85 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Set;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Java21FeaturesTest {
 
-    @Test
-    void sequencedCollection() {
-        Java21Features sut = new Java21Features();
-
-        Set<String> actual = sut.sequencedCollection();
-
-        assertThat(actual).hasSize(2);
+    private static Stream<Arguments> provideArgumentsForPlainSwitch() {
+        return Stream.of(
+                Arguments.of(Java21Features.Dog.of("Max"), "dog Max"),
+                Arguments.of(Java21Features.Cat.of("Tom"), "cat Tom"),
+                Arguments.of(Java21Features.Mouse.of("Jerry"), "mouse")
+        );
     }
 
+    private static Stream<Arguments> provideArgumentsForPlainSwitchWithDefault() {
+        return Stream.of(
+                Arguments.of(Java21Features.Dog.of("Max"), "dog Max"),
+                Arguments.of(Java21Features.Cat.of("Tom"), "cat Tom"),
+                Arguments.of(42, "int 42"),
+                Arguments.of("some text dadada", "unknown")
+        );
+    }
+
+    private static Stream<Arguments> provideArgumentsForPlainSwitchWithNull() {
+        return Stream.of(
+                Arguments.of(Java21Features.Dog.of("Max"), "dog Max"),
+                Arguments.of(Java21Features.Cat.of("Tom"), "cat Tom"),
+                Arguments.of(null, "null"),
+                Arguments.of(42, "unknown")
+        );
+    }
+
+    private static Stream<Arguments> provideArgumentsForPlainSwitchWithCondition() {
+        return Stream.of(
+                Arguments.of(Java21Features.Dog.of("Rex"), "Kommissar Rex"),
+                Arguments.of(Java21Features.Dog.of("Max"), "dog Max"),
+                Arguments.of(Java21Features.Cat.of("Tom"), "cat Tom"),
+                Arguments.of(Java21Features.Mouse.of("Jerry"), "mouse")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForPlainSwitch")
+    void plainSwitch(Java21Features.Animal animal, String expected) {
+        Java21Features sut = new Java21Features();
+
+        String actual = sut.plainSwitch(animal);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForPlainSwitchWithDefault")
+    void plainSwitchWithDefault(Object object, String expected) {
+        Java21Features sut = new Java21Features();
+
+        String actual = sut.plainSwitchWithDefault(object);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForPlainSwitchWithNull")
+    void plainSwitchWithNull(Object object, String expected) {
+        Java21Features sut = new Java21Features();
+
+        String actual = sut.plainSwitchWithNull(object);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForPlainSwitchWithCondition")
+    void plainSwitchWithCondition(Java21Features.Animal animal, String expected) {
+        Java21Features sut = new Java21Features();
+
+        String actual = sut.plainSwitchWithCondition(animal);
+
+        assertEquals(expected, actual);
+    }
 }
